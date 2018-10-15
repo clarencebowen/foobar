46ced750db0fa7b67cb7727309ac859b14499aae
Clarence Bowen <cjb240@nyu.edu>
2018-10-14 21:09:13 -0700

    Fix typo on landing page
    Changed some text to 'No more typos!'
    
    JIRA ticket #124

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          Edit: I was changed!
+          No more typos!
           </p>
           <a
             className="App-link"
